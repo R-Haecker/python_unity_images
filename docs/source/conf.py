171c62a59b3a4e5be5223ceced529f4baab7b4d0
Richard Haecker <richardhaecker1@gmail.com>
2019-12-05 11:19:15 +0100

    get docs working

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,6 +25,7 @@ author = 'Richard Häcker'
 # The full version, including alpha/beta/rc tags
 release = '0.1'
 
+master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
 
