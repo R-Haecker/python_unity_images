87192356865f0471aa57a1a1e40b0c059c56d68b
Richard Haecker <richardhaecker1@gmail.com>
2019-12-09 01:26:34 +0100

    gitignore

--- example.py
+++ example.py
@@ -1,16 +1,7 @@
 import dataset
-data = dataset.dataset_cuboids(dataset_name = "advanced_example", unique_data_folder = False)
-data.reset_index()
+data = dataset.dataset_cuboids(dataset_name = "test_gitignore", unique_data_folder = True)
 dictionaries = []
 for i in range(10):
     dictionaries.append(data.get_example(save_para=True, save_image=True))
 data.exit()
 data.plot_images(dictionaries, images_per_row=5, save_fig=True)
-
-data2 = dataset.dataset_cuboids(dataset_name = "advanced_example", unique_data_folder = False)
-data2.set_config(total_cuboids=[2,3],same_theta=False, DirectionalLightTheta=[80,90], totalPointLights=None, totalSpotLights=None)
-dictionaries2 = []
-for i in range(10):
-    dictionaries2.append(data2.get_example(save_para=True, save_image=True))
-data2.exit()
-data2.plot_images(dictionaries2, images_per_row=5, save_fig=True, show_index=False)
